7056f95010744c4ba843789b7ff9b5cf7ac87795
Alex Trilling <atrill90@gmail.com>
2018-06-05 13:12:47 -0400

    login function started in controller

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,5 @@ const usersController = require("../controllers/usersController");
 
 router.post("/users/register", usersController.create);
 
+router.post("/users/login", usersController.read)
 module.exports = router;
