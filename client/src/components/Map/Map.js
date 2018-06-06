95d0762546583bd5505f31253b53dcd1fb6286c0
Alex Trilling <atrill90@gmail.com>
2018-06-06 14:00:23 -0400

    map moves and zooms and will soon display markers

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -2,11 +2,55 @@ import React,{ Component } from 'react'
 import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps'
 class Map extends Component{
 
+    constructor(){
+        super()
+        this.state = {
+            map:null
+        }
+    }
+
+    mapMoved (){
+        console.log('Map Moved' +JSON.stringify(this.state.map.getCenter()));
+    }
+
+    zoomChanged(){
+    console.log('Zoom Changed');
+    }
+
+    mapLoaded(map){
+        // console.log('mapLoaded: '+JSON.stringify(map.getCenter()))
+        if (this.state.map !=null)
+            return
+
+        this.setState({
+            map:map
+        })
+    }
+
+
+    // componentDidMount(){
+    // this.getLocation();
+    // }
+
+//     getLocation = callback =>{
+//     fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyApLeLjAqhg5nXoDlvzxKzmdv78-Qqh3F8`)
+//     .then(function(response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   });
+// }
+
     render (){
         const markers = this.props.markers || []
         return(
             <div>  Map Component 
             <GoogleMap
+              ref={this.mapLoaded.bind(this)}
+              onDragEnd={this.mapMoved.bind(this)}
+              onZoomChanged={this.zoomChanged.bind(this)}
               defaultZoom={this.props.zoom}
               defaultCenter={ this.props.center }>
               {markers.map((marker,index) => (
