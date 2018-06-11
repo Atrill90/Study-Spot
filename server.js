1ea1347e128df8a831a1ca6fdb4153761870e2ce
Alex Trilling <atrill90@gmail.com>
2018-06-11 17:49:32 -0400

    Building for production

--- server.js
+++ server.js
@@ -35,18 +35,16 @@ if (process.env.NODE_ENV === "production") {
 
 
 // Send every request to the React app
-// Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
-// });
 
 
 
 //Use Routes
-// app.use("/auth",auth);
-// app.use("/api",newSpot);
-
 app.use(require('./routes'));
+
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 
 
